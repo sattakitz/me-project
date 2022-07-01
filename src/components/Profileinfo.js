25418677a77671225aba67c59aa6c16f5221de8c
sattaponz <sattakit.kacs60@ubru.ac.th>
2022-07-02 01:54:35 +0700

    update 7/2/2022

--- src/components/Profileinfo.js
+++ src/components/Profileinfo.js
@@ -4,24 +4,24 @@ function Profileinfo() {
   return (
     <div className="row d-flex vertical-center text-center">
       <div className="col-12 col-md-6 col-lg-6 ">
-        <img src="/me.jpg" className="img-me" alt="logo"></img>
+        <div class="card">
+          <div class="card-item">
+            <img src="/me.jpg" className="img-me" alt="sattapon Me"></img>
+          </div>
+        </div>
       </div>
       <div className="col-12 col-md-6 col-lg-6">
         <div className="myprofile">
-          <div className="item-img">
-            <h1 className="text">SattapoN</h1>
-          </div>
+          <h1 className="text">SattapoN</h1>
         </div>
         <p className="myname"> Name: Sattapon Kamchu </p>
         <div className="myemail">
           <p>E-mail: sattakitkamchu@gmail.com</p>
         </div>
         <div className="typing-demo">
-          <a href="https://usecheck.com/" class="btn-shine" target="_blank">
-            I am a junior web developer.
-          </a>
+          <p class="btn-shine">I am a junior web developer.</p>
         </div>
-        <div className="mysocial">
+        <div className="mysocial mb-4">
           <div className="icon-item">
             <a href="https://www.facebook.com/sattakit.kk/">
               <img src="/facebookicon.png" alt="facebook" />
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2,
+  img = document.getElementsByTagName("img")[0];
+
+function sway(xPos, yPos) {
+  img.style.transform =
+    "rotateX(" + (yPos - wh) / 25 + "deg) rotateY(" + (xPos - ww) / 25 + "deg)";
+}
+
+document.addEventListener("mousemove", function (e) {
+  sway(e.clientX, e.clientY);
+});
+
+document.addEventListener("touchmove", function (e) {
+  e.preventDefault();
+  var touch = e.targetTouches[0];
+  if (touch) {
+    sway(touch.pageX, touch.pageY);
+  }
+});
